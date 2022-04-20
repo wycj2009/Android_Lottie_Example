f8ff2e14aa4a55334824d86157e780dbd7b047e6
wycj2009 <wycj2009@naver.com>
2022-04-21 01:04:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android_lottie_example/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.android_lottie_example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android_lottie_example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // https://airbnb.design/lottie
+        // https://lottiefiles.com
+        binding.lottieAnimationView.apply {
+            setAnimation("loading.json")
+        }.playAnimation()
+    }
+
+}
